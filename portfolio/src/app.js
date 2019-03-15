c06f240216d461dad321bb635412d1e2122f18d3
Mehul Mandviya <mehul@Mehuls-MacBook-Pro.local>
2019-03-15 16:22:49 -0700

    Inintial setup and added basic app component

---
+++ portfolio/src/app.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Hello!</h1>
+                <p>My name is Mehul. I'm a Software Engineer.</p>
+                <p>I'm always looking forward to working on meaningful projects.</p>
+            </div>
+        )
+
+
+    }
+}
+
+export default App;
