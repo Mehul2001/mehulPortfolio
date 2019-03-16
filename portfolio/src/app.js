2b5d79f6311ff15bcf8e9283ae41065de704074e
Mehul Mandviya <mehul@Mehuls-MacBook-Pro.local>
2019-03-15 17:43:51 -0700

    Initialize setstate method to add additional information about a person

--- portfolio/src/app.js
+++ portfolio/src/app.js
@@ -1,12 +1,35 @@
 import React, { Component } from 'react';
 
 class App extends Component {
+    constructor() {
+        super();
+        this.state = { displayBio: false };
+        this.toggleDisplayBio = this.toggleDisplayBio.bind(this);
+    }
+    toggleDisplayBio() {
+        this.setState({ displayBio: !this.state.displayBio });
+    }
     render() {
         return (
             <div>
                 <h1>Hello!</h1>
                 <p>My name is Mehul. I'm a Software Engineer.</p>
                 <p>I'm always looking forward to working on meaningful projects.</p>
+                {this.state.displayBio ? (<div>
+                    <p>
+                        I Live in San Francisco, and code every day.
+            </p>
+                    <p>
+                        My favourite language is JavaScript, and i think React.js is awesome
+            </p>
+                    <p>Besides coding I love hiking and watching Netflix.</p>
+                    <button onClick={this.toggleDisplayBio}>Show Less</button>
+                </div>) : (
+                        <div>
+                            <button onClick={this.toggleDisplayBio}>Read More</button>
+                        </div>
+                    )
+                }
             </div>
         )
 
